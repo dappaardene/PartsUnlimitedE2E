906030830252de3f5824c315590fa34188be20f5
Dappa <mario100scott@gmail.com>
2021-12-29 11:23:12 -0500

    my first commit

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -5,6 +5,8 @@ namespace PartsUnlimited.Models
 {
     public class CartItem : ILineItem
     {
+        //My first change.
+        //Was this saved.
         [Key]
         public int CartItemId { get; set; }
 
